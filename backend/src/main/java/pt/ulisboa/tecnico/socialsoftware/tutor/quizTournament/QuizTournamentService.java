10d5d4f91d0deb09df2056d962c7707a8c715c5f
FranciscoLoureiroTrab <francisco.loureiro@tecnico.ulisboa.pt>
2020-03-06 12:52:43 +0000

    QuizTournamentService added

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/quizTournament/QuizTournamentService.java
@@ -0,0 +1,11 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.quizTournament;
+
+public class QuizTournamentService {
+
+    public void createQuizTournament(){
+    }
+
+    public void studentRegister(){
+
+    }
+}
