164604bdc71c0402699e1db88b5474b18470b9f9
FranciscoLoureiroTrab <francisco.loureiro@tecnico.ulisboa.pt>
2020-03-06 18:47:36 +0000

    CreateQuestionsTournament tests defined, closes #1

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/questionsTournament/service/CreateQuestionsTournamentTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/questionsTournament/service/CreateQuestionsTournamentTest.groovy
@@ -1,13 +1,48 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.questionsTournament.service
 
-import pt.ulisboa.tecnico.socialsoftware.tutor.quizTournament.QuestionsTournamentService
+import pt.ulisboa.tecnico.socialsoftware.tutor.questionsTournament.QuestionsTournamentService
 import spock.lang.Specification
 
-class CreateQuizTournamentTest extends Specification{
+class CreateQuestionsTournamentTest extends Specification{
 
     def questionsTournamentService
 
     def setup(){
-        QuestionsTournamentService questionsTournamentService = new QuestionsTournamentService()
+        questionsTournamentService = new QuestionsTournamentService()
+    }
+
+    def "create questions tournament successfully"(){
+        //creates questions tournament
+        expect:false
+    }
+
+    def "empty starting date"(){
+        //throws exception
+        expect:false
+    }
+
+    def "empty ending date"(){
+        //throws exception
+        expect:false
+    }
+
+    def "starting date greater than ending date"(){
+        //throws exception
+        expect:false
+    }
+
+    def "empty topics"(){
+        //throws exception
+        expect:false
+    }
+
+    def "empty number of question"(){
+        //throws exception
+        expect:false
+    }
+
+    def "user creating tournament is not a student"(){
+        //throw exception
+        expect:false
     }
 }
