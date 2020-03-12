a08959120fe90f4124f43104a4dd8ef41e84b1cf
GoncaloNunoSantos <gonusa@hotmail.com>
2020-03-12 00:00:01 +0000

    Fixing some tests

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/question/service/ChangeQuestionStateTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/question/service/ChangeQuestionStateTest.groovy
@@ -1,151 +1,155 @@
-/*CLASSE PARA FEATURE Ppa2.1 - ChangeQuestionStateTest*/
-
 package groovy.pt.ulisboa.tecnico.socialsoftware.tutor.question.service
 
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Question
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.QuestionService
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Topic
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.QuestionDto
-import pt.ulisboa.tecnico.socialsoftware.tutor.question.repository.QuestionRepository
-
 import pt.ulisboa.tecnico.socialsoftware.tutor.user.User
-import pt.ulisboa.tecnico.socialsoftware.tutor.user.UserRepository
 import pt.ulisboa.tecnico.socialsoftware.tutor.user.dto.UserDto
-
 import pt.ulisboa.tecnico.socialsoftware.tutor.course.Course
 import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseDto
 import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseExecution
-import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseExecutionRepository
-import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseRepository
-import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseService
 import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException
 
 import spock.lang.Specification
 
 
 class ChangeQuestionStateTest extends Specification{
-    static final String COURSE_NAME = "ES"
-    static final String ACRONYM = "QQ"
-    static final String ACADEMIC_TERM = "1 SEM"
-    static final String TOPIC_NAME = "Software Topic"
-    static final String TEACHER_NAME = "Teacher"
+    static final String COURSE_NAME = "Software_Engineering"
+    static final String ACRONYM = "ESoft"
+    static final String ACADEMIC_TERM = "1_SEM"
+
+    static final String TOPIC_NAME = "Topic_Name"
+
+    static final String TEACHER_NAME = "Teacher_Name"
     static final String TEACHER_USERNAME = "Teacher_UserName"
-    static final String STUDENT_NAME = "Student"
-    static final String STUDENT_USERNAME = "Student_UserName"
-    static final String QUESTION_TITLE = "Question about something"
+
+    static final String QUESTION_TITLE = "Question_Title"
     static final String QUESTION_CONTENT = "What is the value of something?"
-    static final String QUESTION_ANSWER = "The value is high."
+    static final String QUESTION_JUSTIFICATION = "The question has no problems"
 
-    static final String QUESTION_JUSTIFICATION = "The question has no problems."
-    static final String CURRENT_STATUS = "PENDING"
+    static final String DEFAULT_STATUS = "PENDING"
+    static final String AVAILABLE_STATUS = "AVAILABLE"
+    static final String DISABLED_STATUS = "DISABLED"
 
-    static final int KEY_TEACHER = 1
-    static final int KEY_STUDENT = 15
+    static final int KEY_TEACHER = 123
+    static final int KEY_QUESTION = 91
 
-    def questionService = new QuestionService()
+    static final int TEACHER_ID = 1
+    static final int QUESTION_ID = 9
+    static final int COURSE_ID = 3
+    static final int COURSE_EXECUTION_ID = 31
+
+
+    //def questionService = new QuestionService()
     def course
     def courseExecution
+    def teacher
     def topic
     def question
     def questionDto
-    def teacher
-    def student
+
 
     def setup(){
         course = new Course(COURSE_NAME, Course.Type.TECNICO)
-        course.setId(1)
-        courseExecution = new CourseExecution(COURSE_NAME as Course, ACRONYM, ACADEMIC_TERM, Course.Type.TECNICO)
+        course.setId(COURSE_ID)
+        courseExecution = new CourseExecution(course, ACRONYM, ACADEMIC_TERM, Course.Type.TECNICO)
+        courseExecution.setId(COURSE_EXECUTION_ID)
         teacher = new User(TEACHER_NAME, TEACHER_USERNAME, KEY_TEACHER, User.Role.TEACHER)
+        teacher.setId(TEACHER_ID)
+        courseExecution.addUser(teacher)
+        course.addCourseExecution(courseExecution)
         topic = new Topic()
         topic.setName(TOPIC_NAME)
         question = new Question()
         question.setCourse(course)
         question.addTopic(topic)
         //question.setHasNoOptions()
-        question.setTitle(topic.getName())
+        question.setTitle(QUESTION_TITLE)
         question.setContent(QUESTION_CONTENT)
-        question.setId(1)
-        question.setKey(123)
+        question.setId(QUESTION_ID)
+        question.setKey(KEY_QUESTION)
         question.setNumberOfAnswers(0)
         question.setNumberOfCorrect(0)
         questionDto = new QuestionDto(question)
-        questionDto.setStatus(CURRENT_STATUS)
+        questionDto.setStatus(DEFAULT_STATUS)
     }
 
-    def "the teacher exists and changes the state of a question and also leaves a comment"(){
-
-        //inicializar
-        given: "add a teacher to a course execution"
-        courseExecution.addUser(teacher)
-        and: "add a course execution to a course"
-        course.addCourseExecution(courseExecution)
+    def "the teacher changes the state of a question to AVAILABLE and leaves a justification"(){
 
         when:
         questionDto.setStatus("AVAILABLE")
         questionDto.setJustification("The question has no problems")
 
-        //verificar
         then:"the returned data is correct"
-        questionDto.getStatus().equals("AVAILABLE")
+        questionDto.getStatus().equals(AVAILABLE_STATUS)
         questionDto.getJustification() == QUESTION_JUSTIFICATION
-        and: "the state of the question was changed"
+        and: "the state of the question was changed in the DataBase"
+        //por implementar com o QuestionService?
     }
 
-    def "change the state of a question to Disabled and leave NULL justification"(){
+    def "the teacher changes the state of a question to AVAILABLE and leaves null justification"(){
+
+        when:
+        questionDto.setStatus("AVAILABLE")
+        questionDto.setJustification("")
+
+        then:"the returned data is correct"
+        questionDto.getStatus().equals(AVAILABLE_STATUS)
+        questionDto.getJustification() == ""
+        and: "the state of the question was changed in the DataBase"
+        //por implementar com o QuestionService?
+    }
+
+
+    def "the teacher changes the state of a question to DISABLED and leaves a justification"(){
+
+        when:
+        questionDto.setStatus("DISABLED")
+        questionDto.setJustification("The question has no problems")
+
+        then:"the returned data is correct"
+        questionDto.getStatus().equals(DISABLED_STATUS)
+        questionDto.getJustification() == QUESTION_JUSTIFICATION
+        and: "the state of the question was changed in the DataBase"
+        //por implementar com o QuestionService?
+    }
+
+
+    def "the teacher changes the state of a question to Disabled and leaves null justification"(){
 
         when: "change state of a question"
-        questionDto.setStatus("DISABLED")
+        questionDto.setStatus(DISABLED_STATUS)
         questionDto.setJustification("")
 
         then: "The exception is thrown"
         thrown(TutorException)
-
     }
 
-    def "change the state of a question to a invalid state"(){
+    def "the teacher changes the state of a question to the invalid state Removed "(){
 
         when: "change question status"
-        questionDto.setStatus("")
+        questionDto.setStatus("REMOVED")
 
         then: "The exception is thrown"
         thrown(TutorException)
     }
 
     /*
-    NAO E NECESSARIA
     def "user is not a teacher"(){
         given: "add a student to a course execution"
         student = new User(STUDENT_NAME, STUDENT_USERNAME, KEY_STUDENT, User.Role.STUDENT)
         courseExecution.addUser(student)
-        and: "add a course execution to a course"
-        course.addCourseExecution(courseExecution)
-
-
         when:
-        def result = student.getRole()
-        result == //role que nao o esperado
-        questionDto.setStatus("AVAILABLE")
-
         then: "Incorrect role and an exception is thrown"
         thrown(TutorException)
-
     }
-    */
 
-    /*
     def "teacher is not in this course"(){
-
         given:
-
         when:
-
         then:
-
     }
     */
-
-    /*def "the question is invalid"(){
-        // questao NULL
-    }*/
 
 }
