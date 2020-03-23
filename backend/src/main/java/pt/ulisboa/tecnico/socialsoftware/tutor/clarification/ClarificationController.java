5058de65a92dc0899bc6bf13b9a33ba8dfb1fb10
dsm - Duarte Matias <duarte.matias@tecnico.ulisboa.pt>
2020-03-23 22:02:03 +0000

    Squashed commit of the following: Closes #38.
    commit 32434dcd08adef4ddf0ee03f7d624665b15b409a
    Author: dsm - Duarte Matias <duarte.matias@tecnico.ulisboa.pt>
    Date:   Mon Mar 23 22:00:37 2020 +0000
    
        Added create clarification answer to clarification controller

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/clarification/ClarificationController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/clarification/ClarificationController.java
@@ -2,11 +2,14 @@ package pt.ulisboa.tecnico.socialsoftware.tutor.clarification;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import pt.ulisboa.tecnico.socialsoftware.tutor.answer.AnswerService;
+import pt.ulisboa.tecnico.socialsoftware.tutor.answer.dto.ClarificationAnswerDto;
 import pt.ulisboa.tecnico.socialsoftware.tutor.clarification.dto.ClarificationDto;
 import pt.ulisboa.tecnico.socialsoftware.tutor.user.dto.UserDto;
 
@@ -16,6 +19,9 @@ import javax.validation.Valid;
 @RestController
 public class ClarificationController {
     private static Logger logger = LoggerFactory.getLogger(ClarificationController.class);
+
+    @Autowired
+    private AnswerService answerService;
 
     private ClarificationService clarificationService;
 
@@ -27,6 +33,17 @@ public class ClarificationController {
                                                 @Valid @RequestBody ClarificationDto clarificationDto,
                                                 @RequestBody UserDto userDto){
         return clarificationService.createClarification(questionAnswerId, clarificationDto, userDto);
+    }
+
+    @PostMapping("/{quizId}/quizAnswer/questionAnswer/clarifications/{clarificationId}/")
+    @PreAuthorize("hasRole('ROLE_TEACHER') and hasPermission(#quizId, 'QUIZ.ACCESS')")
+    public ClarificationAnswerDto createClarificationAnswer(@PathVariable int clarificationId, @PathVariable int quizId,
+                                                            @Valid @RequestBody ClarificationAnswerDto clarificationAnswerDto,
+                                                            @RequestBody UserDto userDto){
+        ClarificationDto clr = new ClarificationDto();
+        clr.setId(clarificationId);
+
+        return answerService.createClarificationAnswer(clr, userDto, clarificationAnswerDto.getAnswer());
     }
 
 }
