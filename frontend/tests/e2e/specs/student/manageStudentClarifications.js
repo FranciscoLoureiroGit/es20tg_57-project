778a3cd465a961dbeb6f20f2836ecb0d3406985b
Daniel <36997776+DanLopess@users.noreply.github.com>
2020-04-14 17:17:48 +0100

    Fixed tests directory

--- frontend/tests/e2e/specs/student/manageStudentClarifications.js
+++ frontend/tests/e2e/specs/student/manageStudentClarifications.js
@@ -51,3 +51,4 @@ describe('Student clarifications walkthrough', () => {
   });
 
 });
+
--- frontend/tests/e2e/support/commands.js
+++ frontend/tests/e2e/support/commands.js
@@ -31,6 +31,12 @@ Cypress.Commands.add('demoAdminLogin', () => {
   cy.contains('Manage Courses').click();
 });
 
+Cypress.Commands.add('demoStudentLogin', () => {
+  cy.visit('/')
+  cy.get('[data-cy="studentButton"]').click()
+  cy.get('[data-cy="quizzesButton"]').click()
+})
+
 Cypress.Commands.add('createCourseExecution', (name, acronym, academicTerm) => {
   cy.get('[data-cy="createButton"]').click();
   cy.get('[data-cy="Name"]').type(name);
@@ -71,3 +77,53 @@ Cypress.Commands.add(
     cy.get('[data-cy="saveButton"]').click();
   }
 );
+
+Cypress.Commands.add('createAndAnswerQuiz', () => {
+  cy.contains('Create').click()
+  cy.get('[data-cy="createButton"]').click()
+  cy.get('[data-cy="endButton"]').click()
+  cy.get('[data-cy="sureButton"]').click()
+})
+
+Cypress.Commands.add('createClarificationRequestFromQuiz', (title, description) => {
+  cy.get('[data-cy="createClarificationButton"]').click()
+  cy.get('[data-cy="title"]').type(title)
+  cy.get('[data-cy="description"]').type(description)
+  cy.get('[data-cy="saveButton"]').click()
+})
+
+Cypress.Commands.add('createClarificationRequestFromSolved', (title, description) => {
+  cy.contains('Solved').click()
+  cy.contains('Generated Quiz').click()
+  cy.get('[data-cy="createClarificationButton"]').click()
+  cy.get('[data-cy="title"]').type(title)
+  cy.get('[data-cy="description"]').type(description)
+  cy.get('[data-cy="saveButton"]').click()
+})
+
+Cypress.Commands.add('showClarificationRequests', () => {
+  cy.get('[data-cy="quizzesButton"]').click()
+  cy.get('[data-cy="clarificationsButton"]').click()
+})
+
+Cypress.Commands.add('openClarificationDescription', (title) => {
+  cy.contains(title)
+    .parent()
+    .should('have.length', 1)
+    .children()
+    .should('have.length', 7)
+    .find('[data-cy="showClarification"]')
+    .click()
+  cy.get('[data-cy="closeButton"]').click()
+})
+
+Cypress.Commands.add('openClarificationQuestion', (title) => {
+  cy.contains(title)
+    .parent()
+    .should('have.length', 1)
+    .children()
+    .should('have.length', 7)
+    .find('[data-cy="showQuestion"]')
+    .click()
+  cy.get('[data-cy="closeButton"]').click()
+})
