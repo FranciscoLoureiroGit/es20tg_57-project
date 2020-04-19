7a1712f143d8564f0c9f46e946bbaaad88359c44
g3n3goncalves <caioagfarias@gmail.com>
2020-04-19 14:02:50 +0100

    Closes #110

--- frontend/cypress/integration/student/createQuestionWithStudent.js
+++ frontend/cypress/integration/student/createQuestionWithStudent.js
@@ -11,7 +11,7 @@ describe('Student question walkthrough', () => {
     cy.createQuestionByStudent('Testing Questions', 'Testing content', 'Answer 1', 'Answer 2', 'Answer 3', 'Answer 4')
   });
 
-  it('once created the question it can checks it', () =>{
+  it('once created the question, the student can list and choose his question', () =>{
     cy.checkQuestionByStudent('Testing Questions')
   })
 
