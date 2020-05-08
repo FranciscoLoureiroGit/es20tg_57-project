39b800070dad1030d603e8baa5b5a74204d730a8
GoncaloNunoSantos <gonusa@hotmail.com>
2020-05-08 14:07:31 +0100

    Refactoring - TopBar

--- frontend/src/components/TopBar.vue
+++ frontend/src/components/TopBar.vue
@@ -277,26 +277,6 @@
           </v-badge>
         </v-btn>
 
-<!--        <v-menu offset-y v-if="isStudent && currentCourse" open-on-hover>
-          <template v-slot:activator="{ on }">
-            <v-btn v-on="on" text dark data-cy="dashboardButton">
-              Dashboard
-              <v-icon>fas fa-file-alt</v-icon>
-            </v-btn>
-          </template>
-          <v-list dense>
-            &lt;!&ndash;NEW&ndash;&gt;
-            <v-list-item to="/student/studentStatistics">
-              <v-list-item-action>
-                <v-icon>assignment</v-icon>
-              </v-list-item-action>
-              <v-list-item-content>
-                <v-list-item-title data-cy="statisticsButton">Question Statistics</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
-        </v-menu>-->
-
         <v-btn
                 v-if="isLoggedIn && moreThanOneCourse"
                 to="/courses"
